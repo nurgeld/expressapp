e7cf78df914d661ec0793047a72250b5a4a3241d
nurgeld <nurgeldy@gmail.com>
2019-08-17 17:40:29 +0600

    fix response in palindrome rout

--- myapp/routes/palindrome.js
+++ myapp/routes/palindrome.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET palindrome */
 router.get('/', function(req, res, next) {
   res.render('palindrome', { title: 'Palindrome' });
+});
+
+router.post('/', function (req, res, next) {
+    console.log(req.body.title);
+    console.log(req.body.word_from_input);
+    console.log(req.body.description);
+    res.send(`'Здесь должен быть ответ - является ли слово палиндромом', ${req.body.word_from_input}`);
 });
 
 module.exports = router;
